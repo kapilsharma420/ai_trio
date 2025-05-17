e606c9d5467f08d09d2161f1e6ae277bbc5da2fa
KAPIL SHARMA <140537615+kapilsharma420@users.noreply.github.com>
2025-05-17 09:18:39 +0530

    refactor: clean up code formatting in chatbot_feature.dart for improved readability

--- lib/screen/features/chatbot_feature.dart
+++ lib/screen/features/chatbot_feature.dart
@@ -20,12 +20,11 @@ class _ChatbotFeatureState extends State<ChatbotFeature> {
       //floating action button i.e text field
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8,),
+        padding: const EdgeInsets.symmetric(horizontal: 8),
         child: Row(
           children: [
             Expanded(
               child: TextFormField(
-                
                 controller: _c.textC,
                 textAlign: TextAlign.center,
                 onTapOutside: (event) => FocusScope.of(context).unfocus(),
@@ -49,9 +48,8 @@ class _ChatbotFeatureState extends State<ChatbotFeature> {
               backgroundColor: Colors.blue,
               radius: 24,
               child: IconButton(
-                onPressed: 
-                  _c.askquestion,
-              
+                onPressed: _c.askquestion,
+
                 icon: Icon(Icons.rocket_launch_rounded),
                 color: Colors.white,
                 iconSize: 28,
@@ -64,14 +62,16 @@ class _ChatbotFeatureState extends State<ChatbotFeature> {
       //body
       body: Obx(
         () => ListView(
-           padding: EdgeInsets.only(
-            bottom: 55, // Add padding to prevent messages from hiding under input field
+          padding: EdgeInsets.only(
+            bottom:
+                55, // Add padding to prevent messages from hiding under input field
             left: 8,
             right: 8,
           ),
           controller: _c.scrollC,
           physics: ScrollPhysics(),
-          children: _c.List.map((e) => MessageCard(message: e)).toList()),
+          children: _c.List.map((e) => MessageCard(message: e)).toList(),
+        ),
       ),
     );
   }
