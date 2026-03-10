b8c1511f8f54adf107d25770d3b439ce4aa7e8ba
KAPIL SHARMA <140537615+kapilsharma420@users.noreply.github.com>
2026-03-10 10:27:00 +0530

    final changes

--- lib/helper/global.dart
+++ lib/helper/global.dart
@@ -1,8 +1,4 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-// gemini api key
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 final String ApiKey = dotenv.env['ApiKey_chat']!;
-
-// Imagine.art API configuration
 final String salabilityAI_API_KEY = dotenv.env['salabilityAI_API_KEY']!;
